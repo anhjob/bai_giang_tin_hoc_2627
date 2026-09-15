011badf68eacd864f4d3c35de5509788791284c2
anhjob <77495612+anhjob@users.noreply.github.com>
2026-09-15 11:20:51 +0700

    ver 1.1

--- Data/list_pdf_links.js
+++ Data/list_pdf_links.js
@@ -1,18 +1,19 @@
 window.DB = {
     K7: [
-        { ten_bai: "👉K7. CHỦ ĐỀ 1", ngay_tao: "--", link: "" ,youtube: "" },
+        { ten_bai: "👉K7. Chủ đề 1. Máy tính và cộng đồng", ngay_tao: "--", link: "" ,youtube: "" },
         { ten_bai: "K7_B01. Thiết bị vào - ra", ngay_tao: "2026-09-10", link: "./K7/K7_1_B01. Thiết bị vào - ra.pdf" ,youtube: "" },
+        { ten_bai: "K7_B02. Phần mềm máy tính", ngay_tao: "2025-11-10", link: "./K7/K7_2_B02. Phần mềm máy tính.pdf" ,youtube: "https://youtu.be/xEFfvfczBz4" },
 
-        { ten_bai: "👉K7. CHỦ ĐỀ 2", ngay_tao: "--", link: "" ,youtube: "" },
-        { ten_bai: "K7_B02. Hệ điều hành", ngay_tao: "2025-11-10", link: "./K7/K7.C.B1.pdf" ,youtube: "https://youtu.be/xEFfvfczBz4" }        
+        { ten_bai: "👉K7. Chủ đề 2. Tổ chức lưu trữ, tìm kiếm và trao đổi thông tin", ngay_tao: "--", link: "" ,youtube: "" },
+        { ten_bai: "K7_B04. Mạng xã hội và một số kênh trao đổi thông tin trên Internet", ngay_tao: "2025-11-10", link: "./K7/K7_2_B04. Mạng xã hội và một số kênh trao đổi thông tin trên Internet.pdf" ,youtube: "https://youtu.be/xEFfvfczBz4" }                
     ],
 
     K9: [
-        { ten_bai: "👉K9. CHỦ ĐỀ 1", ngay_tao: "--", link: "" ,youtube: "" },
+        { ten_bai: "👉K9. Chủ đề 1. Máy tính và cộng đồng", ngay_tao: "--", link: "" ,youtube: "" },
         { ten_bai: "K9_B01. Thế giới kĩ thuật số", ngay_tao: "2026-09-10", link: "./K9/K9_1_B01. Thế giới kĩ thuật số.pdf" ,youtube: "" },
 
-        { ten_bai: "👉K9. CHỦ ĐỀ 2", ngay_tao: "--", link: "" ,youtube: "" },
-        { ten_bai: "K9_B02. Lập trình", ngay_tao: "2025-11-10", link: "./K9/K9.C.B1.pdf" ,youtube: "" }
+        { ten_bai: "👉K9. Chủ đề 2. Tổ chức lưu trữ, tìm kiếm và trao đổi thông tin", ngay_tao: "--", link: "" ,youtube: "" },
+        { ten_bai: "K9_B02. Thông tin trong giải quyết vấn đề", ngay_tao: "2025-11-10", link: "./K9/K9_2_B02. Thông tin trong giải quyết vấn đề.pdf" ,youtube: "" }
     ],
 
     K6: [],
